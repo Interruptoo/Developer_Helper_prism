40bc2c8907e2443e96b9b6ff4bdc133430021b38
DESKTOP-37P1CBQ\dbals <dbalsgh1312@naver.com>
2025-01-25 00:15:16 +0900

    CommonCodeInfo 프로젝트 추가

---
+++ CommonCodeInfo/CommonCodeInfoModule.cs
@@ -0,0 +1,20 @@
+﻿using CommonCodeInfo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace CommonCodeInfo
+{
+    public class CommonCodeInfoModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
